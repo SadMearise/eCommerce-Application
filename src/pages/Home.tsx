58b0b55d96ccf8c27ae95fea29b123a3b52e8c5f
amberlynn <amberlynn364@gmail.com>
2023-08-12 23:24:06 +0300

    feat: add a button that takes you to the registration page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,16 @@
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <Button
+        component={Link}
+        to="/registration"
+      >
+        to Registration
+      </Button>
+    </>
+  );
 }
