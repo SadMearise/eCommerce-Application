0a2d55b7522187caf10d1809511631c45918940c
SadMearise <nikita.khrustalev2012@gmail.com>
2023-08-14 15:40:55 +0300

    feat: add layout to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,18 @@
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+
 export default function Page404() {
-  return <h1>404</h1>;
+  return (
+    <div>
+      <h1>404</h1>
+      <p>We can&apos;t find the page you&apos;re looking for.</p>
+      <Button
+        variant="contained"
+        component={Link}
+        to="/"
+      >
+        Go home
+      </Button>
+    </div>
+  );
 }
