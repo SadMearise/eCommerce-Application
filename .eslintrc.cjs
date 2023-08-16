27060239d8ee67dd722242d6c3917d57d76b9829
amberlynn <amberlynn364@gmail.com>
2023-08-16 18:15:25 +0300

    refactor: update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -96,7 +96,7 @@ module.exports = {
     "no-shadow": "off",
     "@typescript-eslint/no-shadow": "warn",
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error"
+    "@typescript-eslint/no-unused-vars": "error",
   },
   settings: {
     "import/resolver": {
