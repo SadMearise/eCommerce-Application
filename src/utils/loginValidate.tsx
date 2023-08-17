309e3a7b44825edb9cffcaf9a89c5edeb2d8df3f
Yauheniya Mastavaya <jk@db.by>
2023-08-17 15:15:59 +0400

    fix: change password validation rules

--- src/utils/loginValidate.tsx
+++ src/utils/loginValidate.tsx
@@ -14,15 +14,13 @@ export function validatePassword(value: string): string | undefined {
   if (!value) {
     return "Required";
   }
-  const passwordRegex = /^(\S)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^\w\s])(\S).{8,}/;
+
+  const passwordRegex = /^(?! )(?!.* $)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^\w\s]).{8,}$/;
   if (!passwordRegex.test(value)) {
     return (
       "Weak password: mustn't contain leading or trailing whitespace, must be at least 8 characters, " +
       "contain uppercase letter (A-Z), lowercase letter (a-z), digit (0-9), special character (e.g., !@#$%^&*)."
     );
-  }
-  if (value.trim() !== value) {
-    return "Mustn't contain leading or trailing whitespace";
   }
   return undefined;
 }
