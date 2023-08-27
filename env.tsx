135f9df3a067455621ce16f0699d20715ed1a0c6
Yauheniya Mastavaya <jk@db.by>
2023-08-27 19:28:24 +0400

    refactor: remove comments

--- env.tsx
+++ env.tsx
@@ -1,24 +1,3 @@
-// export const CTP_PROJECT_KEY = "kratkoe";
-// export const CTP_CLIENT_SECRET = "kqDUba4Kic80DroP55ZJGf31fPZSo5aM";
-// export const CTP_CLIENT_ID = "dRNDZ4QTkb1DlBx2-nXKWPQ6";
-// export const CTP_AUTH_URL = "https://auth.europe-west1.gcp.commercetools.com";
-// export const CTP_API_URL = "https://api.europe-west1.gcp.commercetools.com";
-// export const CTP_SCOPES = [
-//   "manage_my_orders:kratkoe",
-//   "manage_customers:kratkoe",
-//   "create_anonymous_token:kratkoe",
-//   "view_categories:kratkoe",
-//   "view_discount_codes:kratkoe",
-//   "manage_shopping_lists:kratkoe",
-//   "introspect_oauth_tokens:kratkoe",
-//   "manage_orders:kratkoe",
-//   "manage_my_profile:kratkoe",
-//   "manage_api_clients:kratkoe",
-//   "view_cart_discounts:kratkoe",
-//   "view_published_products:kratkoe",
-//   // "view_products:kratkoe",
-// ];
-
 export const CTP_PROJECT_KEY = "kratkoe";
 export const CTP_CLIENT_SECRET = "LfVCyZ8a_rDAJ6w4o4kp3N2wsSqkUiBK";
 export const CTP_CLIENT_ID = "NV_dUZrM1DgehVEpsJdh4Nq8";
