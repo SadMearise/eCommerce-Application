9c675bf76815d13ad8b239c242c23b7c84b9d71a
amberlynn <amberlynn364@gmail.com>
2023-08-28 19:12:49 +0300

    feat: implement updateCustomerInfo function

--- src/services/customerService.ts
+++ src/services/customerService.ts
@@ -1,4 +1,11 @@
-import { ClientResponse, Customer, CustomerDraft, CustomerSignInResult } from "@commercetools/platform-sdk";
+import {
+  BaseAddress,
+  ClientResponse,
+  Customer,
+  CustomerChangeAddressAction,
+  CustomerDraft,
+  CustomerSignInResult,
+} from "@commercetools/platform-sdk";
 import getApiRoot from "./BuildClient";
 import { ICustomer } from "../models/types";
 import createDraftFromAddress from "../utils/createDraftFromAddress";
@@ -23,16 +30,10 @@ const createCustomer = async (customerData: ICustomer): Promise<ClientResponse<C
   const billingAddressDraft = createDraftFromAddress(billingAddress);
 
   const DEFAULT_SHIPPING_INDEX = 0;
-  let billingIndex = 1;
+  const DEFAULT_BILLING_INDEX = 1;
   const addressesDrafts = [shippingAddressDraft, billingAddressDraft];
-
-  if (billingIsShipping) {
-    billingIndex = 0;
-    addressesDrafts.pop();
-  }
-
   const defaultShippingAddressIndex = defaultShippingAddress ? DEFAULT_SHIPPING_INDEX : undefined;
-  const defaultBillingAddressIndex = defaultBillingAddress ? billingIndex : undefined;
+  const defaultBillingAddressIndex = defaultBillingAddress ? DEFAULT_BILLING_INDEX : undefined;
   const shippingAddressIndex = 0;
   const billingAddressIndex = billingIsShipping ? 0 : 1;
 
@@ -55,6 +56,31 @@ const createCustomer = async (customerData: ICustomer): Promise<ClientResponse<C
 export default createCustomer;
 
 export const getCustomerInfo = async (): Promise<ClientResponse<Customer>> => {
-  const getUser = await getApiRoot().me().get().execute();
+  const getUser = await apiRoot.me().get().execute();
   return getUser;
+};
+
+export const updateCustomerInfo = async (
+  customerID: string,
+  addressVersion: number,
+  addressID: string,
+  address: BaseAddress
+) => {
+  const addressDraft = createDraftFromAddress(address);
+  const actions: CustomerChangeAddressAction = {
+    action: "changeAddress",
+    addressId: addressID,
+    address: addressDraft,
+  };
+
+  return apiRoot
+    .customers()
+    .withId({ ID: customerID })
+    .post({
+      body: {
+        version: addressVersion,
+        actions: [actions],
+      },
+    })
+    .execute();
 };
