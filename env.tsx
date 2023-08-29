453ea8004dfdde676e083ecadbeced93705549af
Yauheniya Mastavaya <jk@db.by>
2023-08-30 00:52:37 +0400

    feat: change api client

--- env.tsx
+++ env.tsx
@@ -1,10 +1,11 @@
 export const CTP_PROJECT_KEY = "kratkoe";
-export const CTP_CLIENT_SECRET = "LfVCyZ8a_rDAJ6w4o4kp3N2wsSqkUiBK";
-export const CTP_CLIENT_ID = "NV_dUZrM1DgehVEpsJdh4Nq8";
+export const CTP_CLIENT_SECRET = "TwWndfm1dmC85O3P1QIiPYQv-NwOECPS";
+export const CTP_CLIENT_ID = "Rfg76YciRVWzwXFElA-83ep8";
 export const CTP_AUTH_URL = "https://auth.europe-west1.gcp.commercetools.com";
 export const CTP_API_URL = "https://api.europe-west1.gcp.commercetools.com";
 export const CTP_SCOPES = [
   "manage_my_orders:kratkoe",
+  "manage_customers:kratkoe",
   "create_anonymous_token:kratkoe",
   "view_categories:kratkoe",
   "manage_my_profile:kratkoe",
