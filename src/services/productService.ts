bbf213c0a0cfa1be13c3e0b980323fd3d0fd3f46
Yauheniya Mastavaya <jk@db.by>
2023-08-29 22:55:49 +0400

    refactor: remove unusable code

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -4,20 +4,11 @@ import StatusCodes from "../types";
 
 export async function getProductByKey(key: string): Promise<ProductProjection> {
   // eslint-disable-next-line newline-per-chained-call
-  // const productType = await getApiRoot()
-  //   .productTypes()
-  //   .withId({ ID: "1560fed2-3628-4dbc-bfe6-62c1153bd41f" })
-  //   .get()
-  //   .execute();
-
-  // eslint-disable-next-line newline-per-chained-call
   const productResp = await getApiRoot().productProjections().withKey({ key }).get().execute();
 
-  // console.log("LOG", productType);
   if (productResp.statusCode !== StatusCodes.Ok) {
     throw new Error("Server error");
   }
-
   const product = productResp.body;
   return product;
 }
@@ -25,15 +16,6 @@ export async function getProductByKey(key: string): Promise<ProductProjection> {
 export async function getProductTypeById(id: string): Promise<ProductType> {
   // eslint-disable-next-line newline-per-chained-call
   const productTypeResp = await getApiRoot().productTypes().withId({ ID: id }).get().execute();
-
-  // eslint-disable-next-line newline-per-chained-call
-  // const productResp = await getApiRoot().productProjections().withKey({ key }).get().execute();
-
-  // console.log("LOG", productType);
-  // if (productResp.statusCode !== StatusCodes.Ok) {
-  //   throw new Error("Server error");
-  // }
-
   const productType = productTypeResp.body;
   return productType;
 }
