3258fe044dec998fda1040acf69f381a3f1fb844
SadMearise <nikita.khrustalev2012@gmail.com>
2023-09-03 19:20:14 +0300

    fix: change props

--- src/services/categories.service.ts
+++ src/services/categories.service.ts
@@ -1,8 +1,10 @@
 import { CategoryPagedQueryResponse } from "@commercetools/platform-sdk";
 import getApiRoot from "./BuildClient";
 
-export default async function getCategories(queryArgs: { expand: string }): Promise<CategoryPagedQueryResponse> {
-  const categoriesResp = await getApiRoot().categories().get({ queryArgs }).execute();
+export default async function getCategories(queryArgs: {
+  queryArgs: { expand: string };
+}): Promise<CategoryPagedQueryResponse> {
+  const categoriesResp = await getApiRoot().categories().get(queryArgs).execute();
   const categories = categoriesResp.body;
 
   return categories;
