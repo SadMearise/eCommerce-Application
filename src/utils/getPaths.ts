12d5a0b0d255d33358dc5fa0ffd93615d5a1b7bb
SadMearise <nikita.khrustalev2012@gmail.com>
2023-09-03 21:43:21 +0300

    fix: change attribute path for query

--- src/utils/getPaths.ts
+++ src/utils/getPaths.ts
@@ -6,9 +6,13 @@ export const getAttributePath = (attribute: string, values: string[]) => {
 
   values.forEach((value: string, index: number) => {
     if (index === values.length - 1) {
+      path += `"${value.toLowerCase()}", `;
+      path += `"${value.toUpperCase()}", `;
       path += `"${setLowerCaseFirstSymbol(value)}", `;
       path += `"${setUpperCaseFirstSymbol(value)}"`;
     } else {
+      path += `"${value.toLowerCase()}", `;
+      path += `"${value.toUpperCase()}", `;
       path += `"${setLowerCaseFirstSymbol(value)}", `;
       path += `"${setUpperCaseFirstSymbol(value)}", `;
     }
