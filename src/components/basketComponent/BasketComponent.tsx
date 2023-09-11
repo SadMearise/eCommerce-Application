aa22456457e553b260d68de957aa4e8487c4dd5e
amberlynn <amberlynn364@gmail.com>
2023-09-11 21:05:06 +0300

    feat: implement BasketComponent component

---
+++ src/components/basketComponent/BasketComponent.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import { ProductProjection } from "@commercetools/platform-sdk";
+import { Button, IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { getProducts } from "../../services/product.service";
+import styles from "./BasketComponent.module.scss";
+import BasketProductCard from "../basketProductCard/BasketProductCard";
+import BasketProductQuantity from "../basketProductQuantity/BasketProductQuantity";
+import BasketProductPrice from "../basketProductPrice/BasketProductPrice";
+import BasketTotalCost from "../basketTotalCost/BasketTotalCost";
+
+export default function BasketComponent() {
+  const [productInBasket] = useState<ProductProjection[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      try {
+        const products: ProductProjection[] = (await getProducts()).body.results;
+        console.log(products);
+
+        products.forEach((item) => {
+          if (productInBasket.length < 3) {
+            productInBasket.push(item);
+          }
+        });
+
+        setIsLoading(false);
+      } catch (e) {
+        console.log(e);
+        setIsLoading(false);
+      }
+    };
+
+    loadProducts();
+  }, []);
+
+  if (isLoading) {
+    return <h1>loading</h1>;
+  }
+
+  return (
+    <div className={styles["basket-wrapper"]}>
+      <div className={styles["left-side"]}>
+        <div className={styles["basket-items-wrapper"]}>
+          <div className={styles["basket-header"]}>
+            <Button>Clear Shopping Cart</Button>
+          </div>
+          {productInBasket.map((product) => (
+            <div
+              key={product.id}
+              className={styles["basket-item-wrapper"]}
+            >
+              <BasketProductCard product={product} />
+              <BasketProductQuantity />
+              <BasketProductPrice product={product} />
+              <IconButton
+                aria-label="delete"
+                sx={{ height: "max-content" }}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={styles["right-side"]}>
+        <div className={styles["sticky-container"]}>
+          <BasketTotalCost products={productInBasket} />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Basket.tsx
+++ src/pages/Basket.tsx
@@ -1,12 +1,13 @@
 import { Container } from "@mui/material";
 import Header from "../components/header/Header";
+import BasketComponent from "../components/basketComponent/BasketComponent";
 
 export default function Basket() {
   return (
     <>
       <Header />
       <Container maxWidth="lg">
-        <h1>Basket</h1>
+        <BasketComponent />
       </Container>
     </>
   );
