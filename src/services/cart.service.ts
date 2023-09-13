5b010e29f29dd70c9fd172654698792e3816dabc
SadMearise <nikita.khrustalev2012@gmail.com>
2023-09-14 01:07:23 +0300

    feat: add cart service

---
+++ src/services/cart.service.ts
@@ -0,0 +1,67 @@
+import { Cart } from "@commercetools/platform-sdk";
+import { CountryCode, StatusCodes } from "../models/types";
+import getApiRoot from "./BuildClient";
+
+export async function getActiveCart(): Promise<false | Cart> {
+  const rootApi = getApiRoot();
+
+  try {
+    const activeCartResp = await rootApi.me().activeCart().get().execute();
+
+    const cart = activeCartResp.body;
+
+    return cart;
+  } catch (err) {
+    if (typeof err === "object" && err && "statusCode" in err && typeof err.statusCode === "number") {
+      if (err.statusCode === StatusCodes.Code404) {
+        return false;
+      }
+    }
+  }
+
+  return false;
+}
+
+export async function createCart() {
+  const rootApi = getApiRoot();
+
+  const cartResp = await rootApi
+    .me()
+    .carts()
+    .post({
+      body: {
+        currency: "EUR",
+        country: CountryCode.Georgia,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .execute();
+
+  const cart = cartResp.body;
+
+  return cart;
+}
+
+export async function addProductToCart(cartId: string, cartVersion: number, productId: string) {
+  const rootApi = getApiRoot();
+
+  await rootApi
+    .me()
+    .carts()
+    .withId({ ID: cartId })
+    .post({
+      body: {
+        version: cartVersion,
+        actions: [
+          {
+            action: "addLineItem",
+            productId,
+            quantity: 1,
+          },
+        ],
+      },
+    })
+    .execute();
+}
