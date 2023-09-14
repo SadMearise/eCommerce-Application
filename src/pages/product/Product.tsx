e8e8db51fedc8fdcf78ab72892285b62accc484e
SadMearise <nikita.khrustalev2012@gmail.com>
2023-09-14 18:48:48 +0300

    feat: add button to product page

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -2,6 +2,7 @@ import { Alert, AlertTitle, CircularProgress, Container } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ProductProjection, ProductType } from "@commercetools/platform-sdk/";
+import Button from "@mui/material/Button";
 import Header from "../../components/header/Header";
 import ProductSlider from "../../components/productSlider/ProductSlider";
 import styles from "./Product.module.scss";
@@ -10,14 +11,15 @@ import { getProductByKey, getProductTypeById } from "../../services/product.serv
 import ProductAttributes from "../../components/productAttributes/ProductAttributes";
 import ProductSizes from "../../components/productSizes/ProductSizes";
 import ProductPrices from "../../components/productPrices/ProductPrices";
+import { getActiveCart } from "../../services/cart.service";
 
 function Product() {
   const params = useParams();
   const [product, setProduct] = useState<ProductProjection>();
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(true);
-
   const [productType, setProductType] = useState<ProductType>();
+  const [isDisabled, setIsDisabled] = useState(false);
 
   useEffect(() => {
     const loadProductAndType = async () => {
@@ -33,11 +35,28 @@ function Product() {
       } catch (e) {
         setError(`Can't load product. ${e}`);
       }
-      setIsLoading(false);
     };
 
     loadProductAndType();
   }, [params.productId]);
+
+  useEffect(() => {
+    const checkCart = async () => {
+      const cart = await getActiveCart();
+
+      if (cart && product) {
+        for (let i = 0; i < cart.lineItems.length; i += 1) {
+          if (cart.lineItems[i].productId === product.id) {
+            setIsDisabled(true);
+            break;
+          }
+        }
+        setIsLoading(false);
+      }
+    };
+
+    checkCart();
+  }, [product]);
 
   if (error) {
     return (
@@ -84,6 +103,12 @@ function Product() {
               productType={productType}
             />
             <ProductSizes product={product} />
+            <Button
+              disabled={isDisabled}
+              variant="outlined"
+            >
+              Add to cart
+            </Button>
           </div>
         </div>
       </Container>
