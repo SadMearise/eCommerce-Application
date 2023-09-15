9f28814b98b25d2fe9c83774c8095097619222ae
amberlynn <amberlynn364@gmail.com>
2023-09-15 23:43:54 +0300

    feat: implement BasketEmptyView component

---
+++ src/components/basketEmptyVIew/BasketEmptyView.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import styles from "./BasketEmptyView.module.scss";
+import RouterPaths from "../../router/routes";
+
+export default function BasketEmptyView() {
+  return (
+    <div className={styles["empty-cart-wrapper"]}>
+      <h2 className={styles["empty-cart-title"]}>Your Cart Is Currently Empty!</h2>
+      <img
+        src="shopping-cart.png"
+        alt="shopping-cart"
+        className={styles["empty-cart-img"]}
+      />
+      <div className={styles["empty-cart-description"]}>
+        <p>Before proceed checkout you must add some products to your shopping cart.</p>
+        <p>You will find a lot of interesting products on our &quot;Catalog&quot; page</p>
+      </div>
+      <Button
+        component={Link}
+        to={RouterPaths.Catalog}
+      >
+        Return to Catalog
+      </Button>
+    </div>
+  );
+}
