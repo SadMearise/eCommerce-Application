34b3271407ef9ffbdf4a87bcf9fb8bbfc39893d3
amberlynn <amberlynn364@gmail.com>
2023-09-15 23:46:47 +0300

    refactor: rebuilds components for shopping cart

--- src/components/basketComponent/BasketComponent.tsx
+++ src/components/basketComponent/BasketComponent.tsx
@@ -1,43 +1,60 @@
 import { useEffect, useState } from "react";
-import { ProductProjection } from "@commercetools/platform-sdk";
-import { Button, IconButton } from "@mui/material";
+import { Cart } from "@commercetools/platform-sdk";
+import { IconButton } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { getProducts } from "../../services/product.service";
 import styles from "./BasketComponent.module.scss";
 import BasketProductCard from "../basketProductCard/BasketProductCard";
 import BasketProductQuantity from "../basketProductQuantity/BasketProductQuantity";
 import BasketProductPrice from "../basketProductPrice/BasketProductPrice";
 import BasketTotalCost from "../basketTotalCost/BasketTotalCost";
 import BasketPromoCodeField from "../basketPromoCodeField/BasketPromoCodeField";
+import { cartDeleteItem, deleteShoppingCart, getShoppingCart } from "../../services/cart.service";
+import BasketEmptyView from "../basketEmptyVIew/BasketEmptyView";
+import BasketClearButton from "../basketClearButton/BasketClearButton";
 
 export default function BasketComponent() {
-  const [productInBasket] = useState<ProductProjection[]>([]);
+  const [shoppingCart, setShoppingCart] = useState<Cart>();
   const [isLoading, setIsLoading] = useState(true);
+
+  const handleUpdateShoppingCart = async () => {
+    const fetchShoppingCart = await getShoppingCart();
+    const [cart] = fetchShoppingCart.body.results;
+    setShoppingCart(cart);
+  };
+
+  const handleDeleteShoppingCartItem = (cartId: string, version: number, itemId: string) => {
+    cartDeleteItem(cartId, version, itemId).then(() => handleUpdateShoppingCart());
+  };
+
+  const handleClearShoppingCart = async () => {
+    if (shoppingCart) {
+      await deleteShoppingCart(shoppingCart.id, shoppingCart.version);
+      handleUpdateShoppingCart();
+    }
+  };
 
   useEffect(() => {
     const loadProducts = async () => {
       try {
-        const products: ProductProjection[] = (await getProducts()).body.results;
-        console.log(products);
-
-        products.forEach((item) => {
-          if (productInBasket.length < 3) {
-            productInBasket.push(item);
-          }
-        });
-
+        const fetchShoppingCart = await getShoppingCart();
+        const [cart] = fetchShoppingCart.body.results;
+        setShoppingCart(cart);
+        console.log(cart);
         setIsLoading(false);
       } catch (e) {
-        console.log(e);
         setIsLoading(false);
+        throw new Error(`${e}`);
       }
     };
 
     loadProducts();
   }, []);
-
   if (isLoading) {
     return <h1>loading</h1>;
+  }
+
+  if ((shoppingCart && shoppingCart?.lineItems.length < 1) || !shoppingCart) {
+    return <BasketEmptyView />;
   }
 
   return (
@@ -45,31 +62,51 @@ export default function BasketComponent() {
       <div className={styles["left-side"]}>
         <div className={styles["basket-items-wrapper"]}>
           <div className={styles["basket-header"]}>
-            <Button>Clear Shopping Cart</Button>
+            <BasketClearButton handleClearShoppingCart={handleClearShoppingCart} />
           </div>
-          {productInBasket.map((product) => (
-            <div
-              key={product.id}
-              className={styles["basket-item-wrapper"]}
-            >
-              <BasketProductCard product={product} />
-              <BasketProductQuantity />
-              <BasketProductPrice product={product} />
-              <IconButton
-                aria-label="delete"
-                sx={{ height: "max-content" }}
+          {shoppingCart &&
+            shoppingCart.lineItems.map((product) => (
+              <div
+                key={product.id}
+                className={styles["basket-item-wrapper"]}
               >
-                <DeleteIcon />
-              </IconButton>
-            </div>
-          ))}
+                <BasketProductCard product={product} />
+                <BasketProductQuantity
+                  product={product}
+                  shoppingCartVersion={shoppingCart.version}
+                  cartId={shoppingCart?.id}
+                  handleUpdateShoppingCart={handleUpdateShoppingCart}
+                />
+                <BasketProductPrice
+                  price={product.price}
+                  productQuantity={product.quantity}
+                />
+                <IconButton
+                  aria-label="delete"
+                  sx={{ height: "max-content" }}
+                  onClick={() => handleDeleteShoppingCartItem(shoppingCart.id, shoppingCart.version, product.id)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </div>
+            ))}
         </div>
       </div>
       <div className={styles["right-side"]}>
         <div className={styles["sticky-container"]}>
           <div className={styles["subtotal-promo-code"]}>
-            <BasketTotalCost products={productInBasket} />
-            <BasketPromoCodeField />
+            {shoppingCart && (
+              <BasketTotalCost
+                discountCodes={shoppingCart.discountCodes}
+                basketItems={shoppingCart.lineItems}
+                totalPrice={shoppingCart.totalPrice}
+              />
+            )}
+            <BasketPromoCodeField
+              shoppingCartID={shoppingCart.id}
+              shoppingCartVersion={shoppingCart.version}
+              handleUpdateShoppingCart={handleUpdateShoppingCart}
+            />
           </div>
         </div>
       </div>
--- src/components/basketPromoCodeField/BasketPromoCodeField.tsx
+++ src/components/basketPromoCodeField/BasketPromoCodeField.tsx
@@ -1,16 +1,37 @@
 import { Button, TextField } from "@mui/material";
+import { useState } from "react";
 import styles from "./BasketPromoCodeField.module.scss";
+import { cartAddDiscount, getDiscount } from "../../services/cart.service";
+import { BasketPromoCodeFieldProps } from "./types";
 
-export default function BasketPromoCodeField() {
+export default function BasketPromoCodeField({
+  shoppingCartID,
+  shoppingCartVersion,
+  handleUpdateShoppingCart,
+}: BasketPromoCodeFieldProps) {
+  const [promoCodeField, setPromoCodeField] = useState("");
+  const handleAddPromoCode = async () => {
+    const discount = (await getDiscount()).body.results;
+    if (discount.some((item) => item.code === promoCodeField)) {
+      cartAddDiscount(shoppingCartID, shoppingCartVersion, promoCodeField).then(() => handleUpdateShoppingCart());
+    }
+  };
   return (
     <div className={styles["promo-code-wrapper"]}>
       <TextField
         id="outlined-helperText"
         label="Enter your promo code"
-        helperText="Добавить промокод для тестирования"
+        helperText="Your promo code is SAVE50"
+        onChange={(e) => setPromoCodeField(e.target.value)}
+        color="warning"
         fullWidth
       />
-      <Button variant="contained">Apply promo code</Button>
+      <Button
+        variant="contained"
+        onClick={handleAddPromoCode}
+      >
+        Apply promo code
+      </Button>
     </div>
   );
 }
