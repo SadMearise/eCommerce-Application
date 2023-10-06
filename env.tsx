948d63f9659b1d18489b0dafd02661a4d1b5bd53
SadMearise <nikita.khrustalev2012@gmail.com>
2023-10-06 05:41:18 +0300

    fix: fix env

--- env.tsx
+++
@@ -1,6 +0,0 @@
-export const CTP_PROJECT_KEY = "kratkoe-n";
-export const CTP_CLIENT_SECRET = "hNJocMnSr7Irzw28PRESdROQvVFb0Ew4";
-export const CTP_CLIENT_ID = "rsutTeWoTFlVxcGuGUG5OgWf";
-export const CTP_AUTH_URL = "https://auth.europe-west1.gcp.commercetools.com";
-export const CTP_API_URL = "https://api.europe-west1.gcp.commercetools.com";
-export const CTP_SCOPES = ["manage_project:kratkoe-n"];
