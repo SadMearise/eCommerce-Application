84e50842990a14a04cf8a2d291e065902e3c8b07
SadMearise <nikita.khrustalev2012@gmail.com>
2023-10-06 19:17:48 +0300

    chore: add condition to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,29 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import topLevelAwait from "vite-plugin-top-level-await";
 
-export default defineConfig({
-  base: "./",
-  resolve: {
-    alias: {
-      process: "process/browser",
-      stream: "stream-browserify",
-      zlib: "browserify-zlib",
-      util: "util",
+export default defineConfig(({ command }) => {
+  const config = {
+    base: "./",
+    resolve: {
+      alias: {
+        process: "process/browser",
+        stream: "stream-browserify",
+        zlib: "browserify-zlib",
+        util: "util",
+      },
     },
-  },
-  plugins: [
-    react(),
-    topLevelAwait({
-      promiseExportName: "__tla",
-      promiseImportName: (i) => `__tla_${i}`,
-    }),
-  ],
+    plugins: [
+      react(),
+      topLevelAwait({
+        promiseExportName: "__tla",
+        promiseImportName: (i) => `__tla_${i}`,
+      }),
+    ],
+  };
+
+  if (command === "build") {
+    config.base = "/eCommerce-Application/";
+  }
+
+  return config;
 });
