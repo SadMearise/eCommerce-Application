5b6c476d62c86e62918828da3562de29dd4d87b2
SadMearise <nikita.khrustalev2012@gmail.com>
2023-10-06 05:52:08 +0300

    refactor: remove test

--- src/tests/sdk.test.tsx
+++
@@ -1,7 +0,0 @@
-import getApiRoot from "../services/BuildClient";
-
-test("Commercetools SDK test", async () => {
-  const apiRoot = getApiRoot();
-  const categories = await apiRoot.categories().get().execute();
-  expect(categories.body.count).toBeGreaterThan(0);
-});
