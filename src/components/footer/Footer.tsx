b6b62296ed60787129a13c01b8e27b1cd428a99a
Nikita Khrustalev <nikita.khrustalev.work@gmail.com>
2025-02-15 00:46:42 +0300

    refactor: add space before return operator

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -14,6 +14,7 @@ function Footer() {
       <span className={styles.nick}>{member.nick}</span>
     </a>
   ));
+
   return (
     <footer className={styles.footer}>
       <Container maxWidth="xl">
